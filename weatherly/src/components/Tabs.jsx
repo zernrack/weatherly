f55a4a93798c94ef11f56fcf09ff783bdf9cb044
Renz Rackhold <67743847+zernrack@users.noreply.github.com>
2023-12-09 16:51:41 +0800

    Change syntax on tabs and props naming

--- weatherly/src/components/Tabs.jsx
+++ weatherly/src/components/Tabs.jsx
@@ -1,16 +1,26 @@
+// Tabs.jsx
 import React from "react";
 import PropTypes from "prop-types";
 
-const Tabs = (props) => {
+const Tabs = ({ name, isActive, onClick }) => {
   return (
     <>
-      <p className="text-2xl font-bold text-[#F0E9E9] hover:cursor-pointer hover:underline ">{props.name}</p>
+      <p
+        className={`text-2xl font-bold text-[#F0E9E9] hover:cursor-pointer ${
+          isActive ? "border-[#F0E9E9] border-b-8 pb-1" : ""
+        }`}
+        onClick={onClick}
+      >
+        {name}
+      </p>
     </>
   );
 };
 
 Tabs.propTypes = {
-    name: PropTypes.string,
+  name: PropTypes.string,
+  isActive: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default Tabs;
